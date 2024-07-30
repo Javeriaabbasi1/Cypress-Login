7836cd6703ffa7120cc022eeed3afd9573ca5480
Javeriaabbasi1 <119102620+Javeriaabbasi1@users.noreply.github.com>
2024-07-30 16:55:02 +0500

    Add files via upload

--- Cypresspractice.cy.js
+++ Cypresspractice.cy.js
@@ -1,4 +1,4 @@
-//Write a Cypress command to visit the URL
+
 describe('Cypress Practice', () => {
     it('visit the URL', () => {
       cy.visit('https://example.com')
@@ -15,4 +15,26 @@ describe('Cypress Practice', () => {
         cy.visit('https://example.com')
         cy.get('.search-box').type('Cypress Testing')
       })
+      it('Wait for 5 seconds before proceeding with the next command', () => {
+        cy.visit('https://example.com')
+        cy.get('.search-box').type('Cypress Testing')
+        cy.wait(5000)
+      })
+      it('Select the option "United States" from a dropdown with the ID "country', () => {
+        cy.visit('https://example.com')
+        cy.get('#country').select('United States')
+      })
+      it('Assert that the element with class "alert" contains the text "Success"', () => {
+        cy.visit('https://example.com')
+        cy.get('.alert').should('contains','Success')
+      })
+      it('Assert that the current URL', () => {
+        cy.visit('https://example.com')
+        cy.get('.alert').should('contains','Success')
+        cy.url().should('eq', 'https://example.com/dashboard');
+     })
+       it('Click on a link with the text "Sign Up"', () => {
+       cy.visit('https://example.com')
+       cy.contains('Sign Up').click()
+     })
 })
